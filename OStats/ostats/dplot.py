f8de7e267f1bc1e31c63d4864d6c1c89a8ce6ed2
OClarkson <ondreaclarkson@gmail.com>
2021-05-21 09:52:30 -0700

    added NBs

--- OStats/ostats/dplot.py
+++ OStats/ostats/dplot.py
@@ -2,3 +2,19 @@ import matplotlib as plt
 import numpy as np
 from distributions import *
 
+
+
+
+
+
+def slr(x,y):
+    if type(x) == list:
+        x = array(x)
+    if type(y) == list:
+        y = array(y)
+    ybar = np.mean(y)
+    xbar = np.mean(x)
+    beta = np.sum((x-xbar)*(y-ybar))/np.sum((x-xbar)**2)
+    alpha = ybar - beta*xbar
+    liney = beta*x + alpha
+    return(liney)
